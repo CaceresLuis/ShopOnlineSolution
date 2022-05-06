030a0fc3eba8fa92e0098db04a0adcc6b364e284
Caceres <lccaceres@outlook.com>
2022-05-06 15:23:39 -0600

    buton update hidden with Js

--- ShopOnline.Web/Pages/ShoppingCartBase.cs
+++ ShopOnline.Web/Pages/ShoppingCartBase.cs
@@ -1,4 +1,5 @@
-﻿using ShopOnline.Models.Dtos;
+﻿using Microsoft.JSInterop;
+using ShopOnline.Models.Dtos;
 using Microsoft.AspNetCore.Components;
 using ShopOnline.Web.Services.Contracts;
 
@@ -6,16 +7,23 @@ namespace ShopOnline.Web.Pages
 {
     public class ShoppingCartBase : ComponentBase
     {
+        
+        [Inject]
+        public IJSRuntime Js { get; set; }
         [Inject]
         public IShoppingCartService ShoppingCartService { get; set; }
         public List<CartItemDto> ShoppingCartItems { get; set; }
         public string ErrorMessenger { get; set; }
+
+        public string TotalPrice { get; set; }
+        public int TotalQuantity { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             try
             {
                 ShoppingCartItems = await ShoppingCartService.GetItems(1);
+                CalculateCartSumaryTotals();
             }
             catch (Exception ex)
             {
@@ -46,6 +54,9 @@ namespace ShopOnline.Web.Pages
                         CartItemId = id
                     };
                     CartItemDto? returnetUpdateItemDto = await ShoppingCartService.UpdateQty(updateItemDto);
+                    UpdateItemTotalPrice(returnetUpdateItemDto);
+                    CalculateCartSumaryTotals();
+                    await MakeUpdateQtyButtonVisible(id, false);
                 }
                 else
                 {
@@ -55,6 +66,8 @@ namespace ShopOnline.Web.Pages
                         item.Qty = 1;
                         item.TotalPrice = item.Price;
                     }
+                    CalculateCartSumaryTotals();
+                    await MakeUpdateQtyButtonVisible(id, false);
                 }
             }
             catch (Exception)
@@ -64,10 +77,46 @@ namespace ShopOnline.Web.Pages
             }
         }
 
+        protected async Task UpdateQty_Input(int id)
+        {
+            await MakeUpdateQtyButtonVisible(id, true);
+        }
+
+        private async Task MakeUpdateQtyButtonVisible(int id, bool visible)
+        {
+            await Js.InvokeVoidAsync("MakeUpdateQtyButtonVisible", id, visible);
+        }
+
+
+        private void UpdateItemTotalPrice(CartItemDto cartItemDto)
+        {
+            var item = GetCartItem(cartItemDto.Id);
+            if (item != null)
+                item.TotalPrice = cartItemDto.Price * cartItemDto.Qty;
+        }
+
+        private void CalculateCartSumaryTotals()
+        {
+            SetTotalPrice();
+            SetTotalQuantity();
+        }
+
+        private void SetTotalPrice()
+        {
+            TotalPrice = ShoppingCartItems.Sum(p => p.TotalPrice).ToString();
+        }
+        
+        private void SetTotalQuantity()
+        {
+            TotalQuantity = ShoppingCartItems.Sum(p => p.Qty);
+        }
+
         private void RemoveCartItem(int id)
         {
             CartItemDto? cartItemDto = GetCartItem(id);
             ShoppingCartItems.Remove(cartItemDto);
+            UpdateItemTotalPrice(cartItemDto);
+            CalculateCartSumaryTotals();
         }
     }
 }
